388b07705532fa927dcce6b1356cacf06d56e4f5
Yasaswini24122000 <yasuabbaraju4@gmail.com>
2025-07-08 14:38:33 +0530

    Create init.py

---
+++ init.py
@@ -0,0 +1,15 @@
+from .core import (
+    PaperRecord,
+    search_papers,
+    save_to_csv,
+    print_to_console
+)
+
+__all__ = [
+    "PaperRecord",
+    "search_papers",
+    "save_to_csv",
+    "print_to_console"
+]
+
+__version__ = "0.1.0"
